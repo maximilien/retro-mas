c950e4f8189af49fe1ced64525e1c1d01e7e04c2
Michael Maximilien <mmaximilien@gmail.com>
2025-04-19 12:02:17 -0700

    first code
    Signed-off-by: Michael Maximilien <mmaximilien@gmail.com>

--- tests/docling_test.py
+++ tests/docling_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 from docling.document_converter import DocumentConverter
 
 from unittest import TestCase
@@ -12,7 +13,8 @@ class TestDocling(TestCase):
         pass
 
     def test_docling_pdf(self):
-        source = 'data/easy_retro_pqca.pdf'
+        source = Path('./data/easy_retro_pqca.pdf')
+        source = "https://arxiv.org/pdf/2408.09869" 
         converter = DocumentConverter()
         result = converter.convert(source)
         markdown = result.document.export_to_markdown()

8d97e687487cad0d54594b2fe386c6f806841bbd
Michael Maximilien <mmaximilien@gmail.com>
2025-04-19 16:59:34 -0700

    cleaned up version for presentation
    Signed-off-by: Michael Maximilien <mmaximilien@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# smol-hack
-Smol hackathon April 2025
+# RetroMas
+
+A a collection of smol agents that will create a summary blog for your EasyRetro or any retro in the format specified below.
+
+This was first created during Smol hackathon April 2025 in San Francisco, CA. Event page: https://lu.ma/0s884wbf?tk=ZCLKn0
+
+# Problem
+
+Retrospective are a means of capturing what went well, what went OK, and what needs improvements after a period of team activity. This is usually done on a weekly basis for agile teams. See [retrospective best practices]() as a good place for the theory and practice.
+
+Tools like [EasyRetro](easyretro.io) do a great job of facilitating running retrospective.
+
+RetroMas is a a multiagent (MAS) solution to help you deal with the data you have collected during your retro. Helping summarize the outcome and eventually creating GitHub issues for your action items.
+
+## Data format
+
+Since there are many tools to run retros (including analog tools like pen and paper, white board and markers) RetroMas assumes a simple CSV format for the data.
+
+```csv
+Went well,Votes Went well,Went OK,Votes Went OK,To Improve,Votes To Improve,Action Items,Votes Action Items,,
+PQCP delivery of mlkem verified implementation. Collaboration between OQS and PQCP,7,Broader participation - more companies since when we started,3,"Need to reach-out to more people for adding more contributors and maintainers. Need more marketing communications: blogs, tweets, LinkedIn. There are some but more is needed",10,"Ask at the board ,meetings to encourage member companies to participate in working groups.
+Specifically ask for talent from the member companies",3,,
+The blog post for the one year anniversary,4,"attracted collab from telco org - didn't adopt code, but worthwhile for all",2,more companies involved in PQCA but not collaborating in meetings/issues?,5,"Create a LinkedIn page. What should we do about other social media: X, BlueSky?",2,,
+...
+```
+
+# Quick testing
+
+You can run and test with the data collected from a [PQCA](https://pqca.org) retrospective in April 2025. The data is in `data/easyretro_pqca.csv`
+
+## Prerequisite
+
+If you want to use the default reasoning models used here you will need to have a [HuggingFace](huggingface.com) account as well as an [OpenAI](openai.com) accounts. 
+
+NOTE: I want to chane this to allow local models but have not done so. If you want to use local models then change the code and PR this repo ;)
+
+Create a `.env` file with your HuggingFace and OpenAI token and API key respectively.
+
+```bash
+HF_TOKEN=[add your hugging face token here]
+OPENAI_API_KEY=[add your OpenAI API key here]
+```
+
+## Running
+
+You can run the RetroMas with the data in this repo: `./data/easyretro_pqca.csv`
+
+```bash
+(smol-hack) ➜  smol-hack git:(main) ✗ uv run python main.py
+Welcome to Retro MAS
+Enter the CSV for your retro data: ./data/easyretro_pqca.csv
+Enter the name of the output blog: blog.md
+```
+
+# Architecture
+
+The architecture uses a team of [smol]() agents to collect data for each of the categories: 'Went well', 'Went OK', and 'To Improve' along with the votes for the entries.
+
+The collected 'Went well', 'Went OK', and 'To Improve' and votes is then summarize by a 'summary' agent which then passes the resul to a 'writer' agent which creates a draft post with the data. The draft is then pass to an 'editor' agent which cleans up and creates the final version of the blog post in markdown.
+
+The following diagram summarises this architecture:
+
+```mermaid
+flowchart LR
+    retro-csv@{ shape: doc, label: "retro.csv (CSV retro data)"}  --> retro-manager@{ label: "RetroManager agent 🤖"}
+
+    retro-manager --> went_well@{label: "went well 🤖"}
+    retro-manager --> went_ok@{label: "went OK 🤖"}
+    retro-manager --> to_improve@{label: "to improve 🤖"}
+
+    went_well --> summary@{label: "summary 🤖"}
+    went_ok --> summary
+    to_improve --> summary
+
+    summary --> writer@{label: "writer 🤖"}
+    writer --> editor@{label: "editor 🤖"}
+
+    editor --> blog_md@{shape: doc, label: "blog.md"}
+```
+
+
+# Next steps
---
+++ retro_mas.py
@@ -0,0 +1,159 @@
+# MIT License
+
+# Copyright (c) 2025 dr.max
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os, csv, json
+
+from dotenv import load_dotenv
+from huggingface_hub import login
+
+from smolagents import (
+    Tool,
+    CodeAgent,
+    DuckDuckGoSearchTool,
+    InferenceClientModel,
+    LiteLLMModel
+)
+
+load_dotenv()
+login(token=os.getenv('HF_TOKEN'))
+
+# tools
+
+class CsvToJson(Tool):
+    description = "Converts CSV content to JSON."
+    name = "csv_to_json"
+    inputs = {"csv_filepath": {"type": "string", "description": "Path to the CSV file to read"}}
+    output_type = "string"
+
+    def forward(self, csv_filepath: str) -> str:
+        data = []
+        with open(csv_filepath, 'r') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                data.append(row)
+        return json.dump(data, file, indent=4)
+
+class FileReader(Tool):
+    description = "Reads the content of a file."
+    name = "file_reader"
+    inputs = {"file_path": {"type": "string", "description": "Path to the file to read"}}
+    output_type = "string"
+
+    def forward(self, file_path: str) -> str:
+        try:
+            with open(file_path, 'r') as file:
+                content = file.read()
+            return content
+        except FileNotFoundError:
+            return f"Error: File not found at {file_path}"
+
+class RetroMas:
+    def __init__(self, data_path='./data/easyretro_pqca.csv', output_file='retro_blog.md'):
+        self.model = LiteLLMModel(model_id="gpt-4o-mini")
+        self.data_path = data_path
+        self.output_file = output_file
+        self._create_agents()
+        self._create_retro_manager()
+
+    def _create_agents(self):
+        self.went_well_agent = CodeAgent(
+            name="went_well",
+            tools=[FileReader()],
+            model = self.model,
+            description="list all rows in 'Went well' with corresponding 'Votes Went well' columns",
+        )
+
+        self.went_ok_agent = CodeAgent(
+            name="went_ok",
+            tools=[FileReader()],
+            model = self.model,
+            description=f"list all rows in 'Went OK' with corresponding 'Votes Went OK' columns",
+        )
+
+        self.to_improve_agent = CodeAgent(
+            name="to_improve",
+            tools=[FileReader()],
+            model = self.model,
+            description=f"list all rows in 'To improve' with corresponding 'Votes To improve' columns",
+        )
+
+        self.summary_agent = CodeAgent(
+            name="summary",
+            tools=[],
+            model = self.model,
+            description="Take the input list of text and votes and create a summary making sure that items that have highest vote is at the top.",
+        )
+
+        self.writer_agent = CodeAgent(
+            name="writer",
+            tools = [],
+            model = self.model,
+            description="Writes blog posts based on the data passed. Provide the research findings and desired tone/style.",
+        )
+
+        self.editor_agent = CodeAgent(
+            name="editor",
+            tools = [],
+            model = self.model,
+            description="Reviews and polishes the blog post based on the research and original task request. Order the final blog post and any lists in a way that is most engaging to someone working in AI. Provides the final, edited version in markdown.",
+        )
+
+    def _create_retro_manager(self):
+        self.retro_manager = CodeAgent(
+            tools=[FileReader()],
+            model=self.model,
+            managed_agents=[self.went_well_agent, self.went_ok_agent, self.to_improve_agent, self.summary_agent, self.writer_agent, self.editor_agent],
+            additional_authorized_imports=["re", "csv", "json"],
+
+            description="""You are a retro manager. Coordinate between went_well, went_ok, to_improve, summary, writer, and editor teams.
+            Follow these steps:
+            1. Use went_well to gather 'Went well' data and 'Went well votes' as went_well_data
+            2. Use went_ok to gather 'Went ok' and 'Went OK votes' as went_ok_data
+            3. Use to_improve to gather 'To Improve' and 'To Improve votes' as to_improve_data
+            4. Pass went_well_data to summary to create went_well_summary
+            5. Pass went_ok_data to summary to create went_ok_summary
+            6. Pass to_improve_data to summary to create to_improve_summary
+            7. Pass went_well_summary, went_ok_summary, and to_improve_summary to writer to create a blog post as draft
+            8. Send draft to editor for final polish
+            9. Save the final markdown file
+            """
+        )
+
+    def retro_summary(self):
+        """
+        Creates a summary of the retro using multiple agents
+
+        Args:
+            input_file (str): the file containing the retro data
+            output_file (str): The filename to save the markdown post
+        """
+        result = self.retro_manager.run(f"""Create a summary of the retro in file: {self.data_path}
+        1. First, get the went_well, went_ok, and to_improve data
+        2. Then, write an engaging summary of each
+        3. Finally, edit and polish the content
+        """)
+
+        with open(self.output_file, 'w', encoding='utf-8') as f:
+            f.write(result)
+        print(f"Blog post has been saved to {self.output_file}")
+
+        return result
